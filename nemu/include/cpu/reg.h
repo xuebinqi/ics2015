73cf1f9d6dcd563d2542548f5f85983d573521ef
tracer-ics2015 <tracer@njuics.org>
2017-08-09 16:59:43 +0800

    >  compile NEMU 141220000 qixuebin Linux qixuebin 4.4.0-89-generic #112-Ubuntu SMP Mon Jul 31 19:37:10 UTC 2017 i686 i686 i686 GNU/Linux  16:59:43 up  1:24,  1 user,  load average: 1.08, 0.84, 0.70 fd56ae9029c412e0d7fbf7c918a94f80ad2ed002

--- nemu/include/cpu/reg.h
+++ nemu/include/cpu/reg.h
@@ -15,15 +15,37 @@ enum { R_AL, R_CL, R_DL, R_BL, R_AH, R_CH, R_DH, R_BH };
  */
 
 typedef struct {
+/* before changed******************
 	struct {
 		uint32_t _32;
 		uint16_t _16;
 		uint8_t _8[2];
 	} gpr[8];
+*********************************/
+//**********my code*****************
+	union {
+		uint32_t _32;
+		uint16_t _16;
+		uint8_t _8[2];
+	} gpr[8];
+//***********************************
 
 	/* Do NOT change the order of the GPRs' definitions. */
 
-	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+//**********before changed**************************
+//	uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+// *************************************************
+
+//**************my code*********************
+	#define eax gpr[0]._32
+	#define ecx gpr[1]._32
+	#define edx gpr[2]._32
+	#define ebx gpr[3]._32
+	#define esp gpr[4]._32
+	#define ebp gpr[5]._32
+	#define esi gpr[6]._32
+	#define edi gpr[7]._32
+//*******************************************
 
 	swaddr_t eip;
 
